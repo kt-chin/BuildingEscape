11ca293a0548a535940232b1cad84d57b3db006d
Kuan-Ting Chin <kt-chin@hotmail.com>
2018-08-24 13:20:42 +0200

    Created Rooms + scripted door to open

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,36 @@
+// Copyright Kuan-Ting Chin 2018
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	FRotator NewRotation = FRotator (0.f, 90.f, 0.f);
+	AActor* Owner = GetOwner(); // Actor variable thats a pointer
+	Owner->SetActorRotation(NewRotation,ETeleportType::None);
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
